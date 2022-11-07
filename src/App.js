a59b2a819770d27a8343dfff687084cec3b76dd0
Oskar Straszynski <oskar.straszynski@gmail.com>
2022-11-07 23:17:03 +0100

    prettier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import Navigation from "./Navigation/Navigation";
 import MainPage from "./MainPage/MainPage";
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-        <MainPage></MainPage>
+      <MainPage></MainPage>
     </div>
   );
 }
--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,32 +1,33 @@
-import {FormControl, InputLabel, Select, TextField} from "@mui/material";
+import { FormControl, InputLabel, Select, TextField } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function MainPage() {
-    const [age, setAge] = useState(10)
-    function handleChange(event,value){
-        setAge(value.props.value)
-    }
+  const [age, setAge] = useState(10);
+  function handleChange(event, value) {
+    setAge(value.props.value);
+  }
 
-    return (
-<Box>
-    <h1>Znajdź prawnika</h1>
-    <h2>Najlepsi prawnicy xd</h2>
-    <FormControl fullWidth>
+  return (
+    <Box>
+      <h1>Znajdź prawnika</h1>
+      <h2>Najlepsi prawnicy xd</h2>
+      <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">Age</InputLabel>
         <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
         >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
         </Select>
-    </FormControl>
-    <TextField id="outlined-search" label="Search field" type="search" />
-</Box>
-    )}
+      </FormControl>
+      <TextField id="outlined-search" label="Search field" type="search" />
+    </Box>
+  );
+}
